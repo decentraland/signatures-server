ff5e219a30c9fe8c0995048018f1d54ae0a00aee
Lautaro Petaccio <lausuper@gmail.com>
2022-06-07 12:37:03 -0300

    fix: Use snake case

--- migrations/1654545715004_rentals.ts
+++ migrations/1654545715004_rentals.ts
@@ -6,27 +6,27 @@ export const shorthands: ColumnDefinitions | undefined = undefined
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createTable("rentals", {
     id: { type: "uuid", notNull: true, primaryKey: true, default: pgm.func("uuid_generate_v4()") },
-    metadataId: { type: "uuid", notNull: true, unique: false, references: "metadata(id)", onDelete: "CASCADE" },
+    metadata_id: { type: "uuid", notNull: true, unique: false, references: "metadata(id)", onDelete: "CASCADE" },
     network: { type: "text", notNull: true },
-    chainId: { type: "string", notNull: true },
+    chain_id: { type: "string", notNull: true },
     expiration: { type: "timestamp", notNull: true },
     signature: { type: "text", notNull: true },
-    rawData: { type: "text", notNull: true },
-    tokenId: { type: "text", notNull: true },
-    contractAddress: { type: "text", notNull: true },
-    rentalContractAddress: { type: "text", notNull: true },
+    raw_data: { type: "text", notNull: true },
+    token_id: { type: "text", notNull: true },
+    contract_address: { type: "text", notNull: true },
+    rental_contract_address: { type: "text", notNull: true },
     lessor: { type: "text", notNull: true },
     tenant: { type: "text", notNull: true },
     status: { type: "text", notNull: true },
-    createdAt: { type: "timestamp", notNull: true, default: pgm.func("now()") },
-    updatedAt: { type: "timestamp", notNull: true, default: pgm.func("now()") },
+    created_at: { type: "timestamp", notNull: true, default: pgm.func("now()") },
+    updated_at: { type: "timestamp", notNull: true, default: pgm.func("now()") },
   })
 
   pgm.createTable("metadata", {
     id: { type: "uuid", notNull: true, primaryKey: true, default: pgm.func("uuid_generate_v4()") },
     category: { type: "text", notNull: true },
-    searchText: { type: "text", notNull: true },
-    createdAt: { type: "timestamp", notNull: true },
+    search_text: { type: "text", notNull: true },
+    created_at: { type: "timestamp", notNull: true },
   })
 
   pgm.createTable("periods", {
@@ -34,11 +34,11 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     min: { type: "integer", notNull: true },
     max: { type: "integer", notNull: true },
     price: { type: "string", notNull: true },
-    rentalId: { type: "uuid", notNull: true, unique: false, references: "rentals(id)", onDelete: "CASCADE" },
+    rental_id: { type: "uuid", notNull: true, unique: false, references: "rentals(id)", onDelete: "CASCADE" },
   })
 
-  pgm.createIndex("periods", "rentalId")
-  pgm.createIndex("rentals", "metadataId")
+  pgm.createIndex("periods", "rental_id")
+  pgm.createIndex("rentals", "metadata_id")
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
