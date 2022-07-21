240367fbdb47f8f170521cc4161e469499a8b712
Lautaro Petaccio <lausuper@gmail.com>
2022-07-21 16:33:52 -0300

    feat: Add new log level configuration for the logger component

--- src/components.ts
+++ src/components.ts
@@ -16,8 +16,9 @@ export async function initComponents(): Promise<AppComponents> {
   const config = await createDotEnvConfigComponent({ path: [".env.default", ".env"] })
   const MARKETPLACE_SUBGRAPH_URL = await config.requireString("MARKETPLACE_SUBGRAPH_URL")
   const RENTALS_SUBGRAPH_URL = await config.requireString("RENTALS_SUBGRAPH_URL")
+  const LOG_LEVEL = await config.requireString("LOG_LEVEL")
 
-  const logs = createLogComponent()
+  const logs = createLogComponent({ config: { logLevel: LOG_LEVEL } })
   const server = await createServerComponent<GlobalContext>({ config, logs }, {})
   const statusChecks = await createStatusCheckComponent({ server, config })
   const fetch = await createFetchComponent()
--- src/metrics.ts
+++ src/metrics.ts
@@ -1,5 +1,6 @@
 import { IMetricsComponent } from "@well-known-components/interfaces"
 import { validateMetricsDeclaration } from "@well-known-components/metrics"
+import { metricDeclarations as loggerMetricsDeclarations } from "@well-known-components/logger"
 
 export const metricDeclarations = {
   test_ping_counter: {
@@ -7,6 +8,7 @@ export const metricDeclarations = {
     type: IMetricsComponent.CounterType,
     labelNames: ["pathname"],
   },
+  ...loggerMetricsDeclarations,
 }
 
 // type assertions
