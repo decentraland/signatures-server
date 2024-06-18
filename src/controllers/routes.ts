9906f56373e81a3e124c173e8f26f03bd44345ee
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-06-18 15:00:56 -0300

    feat: add verify metadata content option

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,15 +1,15 @@
 import { RentalListingCreation } from "@dcl/schemas"
 import { Router } from "@well-known-components/http-server"
 import * as authorizationMiddleware from "decentraland-crypto-middleware"
-import { withSignerValidation } from "../middlewares/withSignerValidation"
 import { GlobalContext } from "../types"
 import { pingHandler } from "./handlers/ping-handler"
 import {
   refreshRentalListingHandler,
   rentalsListingsCreationHandler,
   getRentalsListingsHandler,
-  getRentalListingsPricesHandler
+  getRentalListingsPricesHandler,
 } from "./handlers/rentals-handlers"
+import RequestError from "decentraland-crypto-middleware/lib/errors"
 
 // We return the entire router because it will be easier to test than a whole server
 export async function setupRouter(
@@ -23,9 +23,14 @@ export async function setupRouter(
     "/v1/rentals-listings",
     authorizationMiddleware.wellKnownComponents({
       optional: false,
-      expiration: 5 * 60 * 1000, // 5 minutes
+      expiration: 5 * 60 * 1000, // 5 minutes,
+      verifyMetadataContent: (authMetadata: Record<string, any> | undefined) => {
+        if (authMetadata && authMetadata.signer === "decentraland-kernel-scene") {
+          throw new RequestError("Invalid signer", 400)
+        }
+        return true
+      },
     }),
-    withSignerValidation,
     components.schemaValidator.withSchemaValidatorMiddleware(RentalListingCreation.schema),
     rentalsListingsCreationHandler
   )
